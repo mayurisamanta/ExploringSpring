0bb8acb36d90c85d170d07bab36bba42959da4c0
mayurisamanta <samantamayuri@gmail.com>
2023-08-08 18:09:44 +0530

    adding spring boot project with dependencies

---
+++ SpringBoot-NestedQuery/src/main/java/com/springboot/NestedQuery/SpringBootNestedQueryApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.NestedQuery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNestedQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNestedQueryApplication.class, args);
+	}
+
+}
