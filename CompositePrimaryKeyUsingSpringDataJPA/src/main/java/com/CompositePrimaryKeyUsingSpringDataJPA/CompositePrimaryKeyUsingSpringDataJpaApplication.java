29997cff8869008effa05466da46e51d9d11479b
mayurisamanta <samantamayuri@gmail.com>
2023-08-11 13:59:13 +0530

    adding spring boot project with dependencies

---
+++ CompositePrimaryKeyUsingSpringDataJPA/src/main/java/com/CompositePrimaryKeyUsingSpringDataJPA/CompositePrimaryKeyUsingSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.CompositePrimaryKeyUsingSpringDataJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompositePrimaryKeyUsingSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompositePrimaryKeyUsingSpringDataJpaApplication.class, args);
+	}
+
+}
